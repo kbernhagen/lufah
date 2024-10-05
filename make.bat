2597673dd10f48137ad683e4e94675d20fb564a7
Kevin Bernhagen <kbernhagen.github@gmail.com>
2024-10-04 17:08:10 -0700

    Update Windows make.bat

--- make.bat
+++ make.bat
@@ -8,14 +8,15 @@ if "%1" == "" goto help
 if "%1" == "help" (
 	:help
 	echo.Please use `make ^<target^>` where ^<target^> is one of
-	echo.  install-user  install via pipx for current user
+	echo.  install-user    install via pipx for current user
 	echo.  uninstall-user  uninstall via pipx for current user
+	echo.  install-uv      install uv via pipx for current user
 	goto end
 )
 
 if "%1" == "install-user" (
-	echo. py -m venv %VENV%
-	py -m venv %VENV%
+	echo. python -m venv %VENV%
+	python -m venv %VENV%
 	%PYTHON% -m ensurepip
 	%PYTHON% -m pip install pipx
 	%PYTHON% -m pipx install --force .
@@ -29,6 +30,17 @@ if "%1" == "uninstall-user" (
 	%PYTHON% -m ensurepip
 	%PYTHON% -m pip install pipx
 	%PYTHON% -m pipx uninstall lufah
+	goto end
+)
+
+if "%1" == "install-uv" (
+	echo. python -m venv %VENV%
+	python -m venv %VENV%
+	%PYTHON% -m ensurepip
+	%PYTHON% -m pip install pipx
+	%PYTHON% -m pipx install uv
+	%PYTHON% -m pipx ensurepath
+	echo. You may need to open a new terminal to use uv
 	goto end
 )
 
